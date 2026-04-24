009b92e5dbb49992884e72ba8f3d744b1148f9cc
layaneSouza <layane.fsouza14@gmail.com>
2026-04-24 08:53:37 -0300

    Aposentadoria atualizada

--- app/src/main/java/com/example/variaveis/MainActivity.kt
+++ app/src/main/java/com/example/variaveis/MainActivity.kt
@@ -1,26 +1,59 @@
 package com.example.variaveis
 
 import android.os.Bundle
+import android.util.Log
 import android.view.inputmethod.InputBinding
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.variaveis.databinding.ActivityMainBinding
+import kotlin.math.log
 
-//Variável imutável: não pode ser alterada nem em tempo de execução
-const val VERSAO_DO_APP = "1.0.0"
+const val indicemasculino = 65
+const val indicefeminino = 62
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonMostrar.setOnClickListener {
-            val nomeCompleto = binding.ediTextNome.text.toString()
-            binding.textViewResultado.text = nomeCompleto
+        val items = listOf("Masculino", "Feminino")
+        val adapter= ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+
+        binding.spinnerSexo.adapter = adapter
+
+
+        binding.buttonCalcular.setOnClickListener {
+            val sexoselecionado = binding.spinnerSexo.selectedItem  as String
+            val idade = binding.ediTextNome.text.toString().toLongOrNull()
+
+            var resultado: Long=0L
+
+            if (idade!= null){
+
+                if(sexoselecionado.trim()=="Masculino") {
+                    if(indicemasculino>idade){
+                        resultado = indicemasculino - idade
+                    }
+                }
+                else{if (indicefeminino>idade){
+                    resultado= indicefeminino - idade
+                    }
+                }
+                if (resultado!=0L){
+                    binding.textViewResultado.text="Faltam $resultado para você se aposentar"
+                }else{
+                    binding.textViewResultado.text="Você já pode se aposentar"
+                }
+            }else{
+                binding.textViewResultado.text="Informe sua idade"
+            }
+
         }
 
     }
